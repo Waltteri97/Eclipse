7d519e10dcdff6797c351f171b76864e9245d264
Eerozi <eero.salminen@outlook.com>
2020-10-04 21:39:45 +0300

    Eeron muutoksia /mm. addorder jne.

--- pizza-app/src/main/java/services/PizzaService.java
+++ pizza-app/src/main/java/services/PizzaService.java
@@ -27,6 +27,8 @@ import com.google.appengine.api.utils.SystemProperty;
 import java.sql.Statement;
 import conn.Connections;
 import data.Pizza;
+import data.Order;
+
 
 
 @Path("/pizzaservice")
@@ -71,6 +73,58 @@ public class PizzaService {
 		   e.printStackTrace();
 	   }
 	   return pizza;
+	   
+	   
+	   
+	   }
+	   
+	   
+	   @POST
+	   @Produces(MediaType.APPLICATION_JSON)
+	   @Consumes("application/x-www-form-urlencoded")
+       @Path("/addorder")
+	   public Order addOrderByPost(@FormParam("pizza") String pizza, @FormParam("price") float price, @FormParam("first_name") String first_name, @FormParam("last_name") String last_name, @FormParam("email") String email, @FormParam("phonenumber") String phonenumber, @FormParam("address") String address) {
+		   Order order=new Order();
+		   order.setPizza(pizza);
+		   order.setPrice(price);
+		   order.setFirstname(first_name);
+		   order.setLastname(last_name);
+		   order.setEmail(email);
+		   order.setPhonenumber(phonenumber);
+		   order.setAddress(address);
+		   String sql="insert into orders(pizza, price, first_name, last_name, email, phonenumber, address) values(?,?,?,?,?,?,?)";
+		   
+		 
+		   
+	   Connection conn=null;   
+	   try {
+		   if(SystemProperty.environment.value()  == SystemProperty.Environment.Value.Production) {
+			   conn = Connections.getProductionConnection();
+		   }
+		   else {
+			   conn = Connections.getDevConnection();
+		   }
+		  
+	   }   catch (Exception e) {
+		   
+		   e.printStackTrace();
+	   }
+	   PreparedStatement pstmt;
+	   try {
+		   pstmt = conn.prepareStatement(sql);
+		   pstmt.setString(1, pizza);
+		   pstmt.setFloat(2, price);
+		   pstmt.setString(3, first_name);
+		   pstmt.setString(4, last_name);
+		   pstmt.setString(5, email);
+		   pstmt.setString(6, phonenumber);
+		   pstmt.setString(7, address);
+
+		   pstmt.execute();
+	   } catch (SQLException e) {
+		   e.printStackTrace();
+	   }
+	   return order;
 	   
 	   
 	   
