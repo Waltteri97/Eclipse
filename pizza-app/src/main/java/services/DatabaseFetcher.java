bc99d5d8123529d1f288accb02c2658f64c982e7
sundeka <sundellkalle@gmail.com>
2020-10-06 10:58:08 +0300

    getAllMenus -metodin luonti

--- pizza-app/src/main/java/services/DatabaseFetcher.java
+++ pizza-app/src/main/java/services/DatabaseFetcher.java
@@ -15,6 +15,7 @@ import com.google.appengine.api.utils.SystemProperty;
 
 import conn.ConnectionsPizza;
 import data.Topping;
+import data.Menu;
 
 @Path("/db")
 public class DatabaseFetcher {  
@@ -61,5 +62,53 @@ public class DatabaseFetcher {
 		    	}
 		    }
 		    return list;
+	    }
+	    
+	    
+	    @GET
+	    @Produces(MediaType.APPLICATION_JSON)
+	    @Path("/getAllMenus")
+	    public ArrayList<Menu> getMenus() {
+	    	String sql = "select * from menus";
+	    	ResultSet RS = null;
+	    	ArrayList<Menu> list = new ArrayList<>();
+	    	
+	    	Connection conn=null;   
+		    try {
+			    if (SystemProperty.environment.value()  == SystemProperty.Environment.Value.Production) {
+				    conn = ConnectionsPizza.getProductionConnection();
+			    } else {
+				    conn = ConnectionsPizza.getDevConnection();
+			    }
+		    } catch (Exception e) {	    
+			    e.printStackTrace();
+		    }
+		    
+		    Statement stmt;
+		    try {
+		    	stmt = conn.createStatement();
+		    	RS=stmt.executeQuery(sql);
+		    	while (RS.next()) {
+		    		Menu m = new Menu();
+		    		m.setId(RS.getInt("id"));		    		
+		    		m.setTayte1(RS.getString("tayte1"));
+		    		m.setTayte2(RS.getString("tayte2"));
+		    		m.setTayte3(RS.getString("tayte3"));
+		    		m.setPrice(RS.getDouble("price"));
+		    		m.setRestaurant(RS.getString("restaurant"));
+		    		list.add(m);
+		    	}
+		    } catch (SQLException e) {
+		    	e.printStackTrace();
+		    }
+		    
+		    if (conn!=null) {
+		    	try {
+		    		conn.close();
+		    	} catch (SQLException e) {
+		    		e.printStackTrace();
+		    	}
+		    }
+	    	return list;
 	    }
 }
